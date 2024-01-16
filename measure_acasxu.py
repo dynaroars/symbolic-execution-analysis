c6e916505e06a222efe3beae855b5b0c25d646e9
hodangkhoinguyen <50374959+hodangkhoinguyen@users.noreply.github.com>
2024-01-15 16:28:45 -0800

    Read ACAS Xu .nnet and spec files

---
+++ measure_acasxu.py
@@ -0,0 +1,117 @@
+from keras.models import Sequential
+from keras.layers import Dense
+from keras import activations
+import tensorflow as tf
+import z3
+
+def read_spec(filename: str):
+    file = open(filename)
+    variable_dict = dict()
+    constraint_list = []
+    for line in file:
+        line = line.strip()
+
+        # Skip empty or comment lines
+        if line == "" or line.startswith(";"):
+            continue
+
+        line = line[1:-1]
+        if line.startswith("declare-const"):
+            parse_line = line.split()
+            variable_name = parse_line[1]
+            type = parse_line[2]
+            if type == "Real":
+                variable_dict[variable_name] = z3.Real(variable_name)
+        elif line.startswith("assert"):
+            op, variable_name, value = line[8:-1].split()
+            if op == ">=":
+                constraint_list.append(variable_dict[variable_name] >= eval(value))
+            elif op == "<=":
+                constraint_list.append(variable_dict[variable_name] >= eval(value))
+
+    file.close()
+    return z3.And(constraint_list)
+
+print(read_spec('acasxu/spec/prop_1.vnnlib'))
+
+def readNNet(filename: str) -> Sequential:
+    """
+    Return a DNN based on the information from the provided file
+    Args:
+        file (type: str): filename that provides information about NNet
+    """
+    file = open(filename)
+
+    # Skip header lines
+    line = file.readline()
+    while line.startswith("//"):
+        line = file.readline()
+
+    parse_line = line.split(",")
+    num_layers = int(parse_line[0])
+    num_inputs = int(parse_line[1])
+
+    line = file.readline()
+    parse_line = line.split(",")
+
+    layers = []
+    for i in range(num_layers + 1):
+        layers.append(int(parse_line[i]))
+
+    # Skip line containing "0,"
+    file.readline()
+
+    # Read the normalization information
+    line = file.readline()
+    inputMins = [float(x) for x in line.strip().split(",")[:-1]]
+
+    line = file.readline()
+    inputMaxes = [float(x) for x in line.strip().split(",")[:-1]]
+
+    line = file.readline()
+    means = [float(x) for x in line.strip().split(",")[:-1]]
+
+    line = file.readline()
+    ranges = [float(x) for x in line.strip().split(",")[:-1]]
+
+    model = Sequential()
+    for i in range(1, num_layers + 1):
+        nodes = layers[i]
+        prev_nodes = layers[i - 1]
+
+        weights = [[] for x in range(prev_nodes)]
+        biases = []
+
+        for _ in range(nodes):
+            line = file.readline()
+            weight_list = [float(x) for x in line.strip().split(",")[:-1]]
+            for w in range(prev_nodes):
+                weights[w].append(weight_list[w])
+
+        for _ in range(nodes):
+            line = file.readline()
+            biases.append([float(line.strip().split(",")[0])])
+        if i == 1:
+            dense = Dense(units = nodes,
+                        input_shape = (num_inputs, ),
+                        kernel_initializer = tf.constant_initializer(weights),
+                        bias_initializer = tf.constant_initializer(biases),
+                        dtype='float64')
+        # the output layer has no activation function
+        elif i == num_layers:
+            dense = Dense(units = nodes,
+                activation = None,
+                kernel_initializer = tf.constant_initializer(weights),
+                bias_initializer = tf.constant_initializer(biases),
+                dtype='float64')
+        # the rest
+        else:
+            dense = Dense(units = nodes,
+                activation = activations.relu,
+                kernel_initializer = tf.constant_initializer(weights),
+                bias_initializer = tf.constant_initializer(biases),
+                dtype='float64')
+
+        model.add(dense)
+    file.close()
+    return model
